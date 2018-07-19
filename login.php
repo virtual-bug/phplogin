eb9ba44044a0e91005d384f009df2947d16edf50
Mostafa Ibrahem <mostafa.ibrahem@cis.asu.edu.eg>
2018-07-18 23:18:19 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login page </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="frm">
+            <form action="process.php" method="POST">
+                <p>
+                    <label>User_Name</label>
+                    <input type="text" id="user" name="user">
+                    
+                </p>
+                 <p>
+                    <label>password</label>
+                    <input type="text" id="password" name="password">
+                    
+                </p>
+                 <p>
+                     
+                     <input type="submit" id="btn" name="login">
+                    
+                </p>
+            </form>
+            
+        </div>
+    </body>
+    
+</html>
+ 
+ 
+
