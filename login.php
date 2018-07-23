10e95f1c6f5bebf7b7c923e91083005417bdafff
Mostafa Ibrahem <mostafa.ibrahem@cis.asu.edu.eg>
2018-07-23 09:11:12 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -1,33 +1,31 @@
+<?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Login page </title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
-        <div id="frm">
-            <form action="process.php" method="POST">
-                <p>
-                    <label>User_Name</label>
-                    <input type="text" id="user" name="user">
-                    
-                </p>
-                 <p>
-                    <label>password</label>
-                    <input type="text" id="password" name="password">
-                    
-                </p>
-                 <p>
-                     
-                     <input type="submit" id="btn" name="login">
-                    
-                </p>
-            </form>
-            
-        </div>
-    </body>
-    
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
 </html>
- 
- 
-
